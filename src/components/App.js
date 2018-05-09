98653c1c9eb1699905f2664c84e990dd02df18bc
Jonathan Waller <waller.j27@gmail.com>
2018-05-09 00:27:44 -0500

    step 1 complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
 import './App.css';
 
 import Header from './Header/Header';
 import Compose from './Compose/Compose';
+import Post from './Post/Post'
 
 class App extends Component {
   constructor() {
@@ -18,8 +19,12 @@ class App extends Component {
     this.createPost = this.createPost.bind( this );
   }
   
-  componentDidMount() {
-
+  componentDidMount(){
+    axios.get('https://practiceapi.devmountain.com/api/posts')
+    .then(results=>{
+      // console.log(results);
+      this.setState({posts:results.data})
+    })
   }
 
   updatePost() {
@@ -44,7 +49,13 @@ class App extends Component {
         <section className="App__content">
 
           <Compose />
-          
+          {
+            posts.map((currPost)=>
+          <Post key={currPost.id} />)
+          }
+
+
+
         </section>
       </div>
     );
